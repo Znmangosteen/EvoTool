f3933739a5bd10b740faa7c28bcd9ecf98f1e61e
Znmangosteen <34234009+Znmangosteen@users.noreply.github.com>
2021-06-07 17:12:54 +0800

    cont. classification

--- algorithm/mertric.py
+++ algorithm/mertric.py
@@ -1,8 +1,12 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score,precision_score ,recall_score, log_loss,confusion_matrix
 
 
 def eval_model(model, data_x, data_y, ):
+    model_pred = model.predict(data_x)
+    return np.square(mean_squared_error(data_y, model_pred)), r2_score(data_y, model_pred)
+
+def eval_model_classification(model, data_x, data_y, ):
     model_pred = model.predict(data_x)
     return np.square(mean_squared_error(data_y, model_pred)), r2_score(data_y, model_pred)
 
--- algorithm/random_forest_encap.py
+++ algorithm/random_forest_encap.py
@@ -10,28 +10,50 @@ class random_forest_model(prediction_model):
     def __init__(self, params, model_type='regression'):
         super(random_forest_model, self).__init__()
         self.params = params
+        self.model_type = model_type
         self.model = self.MODEL_POLL[model_type](**params)
 
     def train(self, X_train, y_train, X_val=None, y_val=None):
         self.model.fit(X_train, y_train)
-        rmse_train = []
-        rmse_val = []
-        r2_train = []
-        r2_val = []
 
-        es_bk = self.model.estimators_
-        for i in range(len(self.model.estimators_)):
-            rmse, r2 = eval_model(self, X_train, y_train)
-            rmse_train = [rmse] + rmse_train
-            r2_train = [r2] + r2_train
+        if self.model_type=='regression':
 
-            rmse, r2 = eval_model(self, X_val, y_val)
-            rmse_val = [rmse] + rmse_val
-            r2_val = [r2] + r2_val
+            rmse_train = []
+            rmse_val = []
+            r2_train = []
+            r2_val = []
 
-            self.model.estimators_ = self.model.estimators_[:-1]
-        self.model.estimators_ = es_bk
-        return rmse_train, r2_train, rmse_val, r2_val
+            es_bk = self.model.estimators_
+            for i in range(len(self.model.estimators_)):
+                rmse, r2 = eval_model(self, X_train, y_train)
+                rmse_train = [rmse] + rmse_train
+                r2_train = [r2] + r2_train
 
+                rmse, r2 = eval_model(self, X_val, y_val)
+                rmse_val = [rmse] + rmse_val
+                r2_val = [r2] + r2_val
+
+                self.model.estimators_ = self.model.estimators_[:-1]
+            self.model.estimators_ = es_bk
+            return rmse_train, r2_train, rmse_val, r2_val
+        elif self.model_type=='classification':
+            rmse_train = []
+            rmse_val = []
+            r2_train = []
+            r2_val = []
+
+            es_bk = self.model.estimators_
+            for i in range(len(self.model.estimators_)):
+                rmse, r2 = eval_model(self, X_train, y_train)
+                rmse_train = [rmse] + rmse_train
+                r2_train = [r2] + r2_train
+
+                rmse, r2 = eval_model(self, X_val, y_val)
+                rmse_val = [rmse] + rmse_val
+                r2_val = [r2] + r2_val
+
+                self.model.estimators_ = self.model.estimators_[:-1]
+            self.model.estimators_ = es_bk
+            return rmse_train, r2_train, rmse_val, r2_val
     def get_feature_importance(self):
         return self.model.feature_importances_
